76a471964199f68a345de139eef53f4131e44d5b
Eric Schneider <franz.eric.schneider@gmail.com>
2015-10-07 20:49:06 -0400

    Tested rudder reading code

--- rudder_teensy/rudder_teensy.ino
+++ rudder_teensy/rudder_teensy.ino
@@ -1,6 +1,6 @@
 const int encoderPin = A0;          // input pin for the potentiometer
 int rawEncoderVal = 0;              // raw analog read value
-const int rawEncoderValToRadians = 2 * 3.14159 / 1024.0;
+const float rawEncoderValToRadians = 2 * 3.14159 / 1024.0;
 float encoderAngleRadians = 0.0;    // tracks current rudder angle
 float encoderAngleZeroOffset = 0.0; // we'll want to set the "zerod" position of the rudder
 
@@ -17,7 +17,7 @@ void loop() {
     // Print useful debug information
     Serial.println("Debug information:");
     Serial.print("Raw rudder value: ");
-    Serial.println(rawEncoderVal);
-    Serial.print("Rudder angle value: ");
-    Serial.println(encoderAngleRadians);
+    Serial.print(rawEncoderVal);
+    Serial.print("\tRudder angle value: ");
+    Serial.print(encoderAngleRadians);
 }
