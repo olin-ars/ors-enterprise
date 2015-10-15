5f40352971c1d0df9df4f3f68057137c4f498836
Eric Schneider <franz.eric.schneider@gmail.com>
2015-10-15 16:30:48 -0400

    In the middle of testing this test

--- sail_teensy/tests/test_hall_effect_sensors/test_hall_effect_sensors.ino
+++ sail_teensy/tests/test_hall_effect_sensors/test_hall_effect_sensors.ino
@@ -1,13 +1,25 @@
 #define arrayLength 7 //used everywhere except line 14, I couldn;t figure out how to make that line better
-int hallVals[] = {0, 0, 0, 0, 0, 0, 0}; //used to store which hall eefect sensors are currently active
+#define raAlpha 0.1 //used for running average to weight current data and average data
+#define triggerVal 1.1 //used as margin that sensor val must be over in order to count for sensor activating
+int hallVals[] = {0, 0, 0, 0, 0, 0, 0}; //used to store which hall effect sensors are currently active
+float hallAvgs[] = {0, 0, 0, 0, 0, 0}; //used to store running averages of past hall sensor data
 int lastHallVals[] = {0, 0, 0, 0, 0, 0, 0}; //used to store which hall was last active
 int dir = -1; //used to store the linear actuator direction
+
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  // For some reason analogRead(6) wasn't working inside setup, so we
+  // had to move this to the loop beginning
+  for(int i = 0; i < arrayLength; i++){
+    if (hallAvgs[i] == 0) {
+      hallAvgs[i] = analogRead(i);
+    }
+  }
+
   readHalls();
   printHallAndDirection();
   
@@ -15,18 +27,20 @@ void loop() {
     //this if is to check if there is an active hall sensor, if there isn't then we don't want to change the array holding the last active one
     memcpy(hallVals, lastHallVals, arrayLength); //must be after both readHalls() and returnDirection()
   }
-  delay(5);
+  delay(25);
 }
 
 void readHalls(){ //reads all the hall sensors, if any are active (giving a value of more than 800) it stores a 1 in that sensors position in the active array
   for(int i = 0; i < arrayLength; i++){
-    hallVals[i] = analogRead(i);
-    if(hallVals[i] > 800){
+    int readVal = analogRead(i);
+          Serial.print("\t\t");
+          Serial.print(readVal-hallAvgs[i]);
+    if(readVal-hallAvgs[i] >= triggerVal){
       hallVals[i] = 1;
-    }
-    else{
+    } else if(readVal-hallAvgs[i] <= -triggerVal){
       hallVals[i] = 0;
     }
+    hallAvgs[i] = ((1-raAlpha)*hallAvgs[i])+(raAlpha*readVal);
   }
 }
 
@@ -63,10 +77,18 @@ int returnDirection(){ //will return 1 if moving forward, 0 if moving backwards,
 }
 
 void printHallAndDirection(){ //if there's and active hall, this prints which is active and the direction. If none are active it prints the direction.
+  Serial.println("");
+  Serial.print("Hall Vals:");
+  for (int i=0; i<arrayLength; i++) {
+    Serial.print(hallVals[i]);
+    Serial.print("\t\t");
+  }
+  Serial.println("");
+
   if(returnActiveHall() > -1){
     Serial.print("Hall Sensor ");
     Serial.print(returnActiveHall());
-    Serial.print("is activated, Direction is ");
+    Serial.print(" is activated, Direction is ");
     Serial.println(returnDirection());
   }
   else{
@@ -74,3 +96,4 @@ void printHallAndDirection(){ //if there's and active hall, this prints which is
     Serial.println(returnDirection());
   }
 }
+
