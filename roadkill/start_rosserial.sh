934bf397c6dc91e4e07a827a04ff88a8c6ef63aa
Eric Miller <eric@legoaces.org>
2015-10-29 18:15:01 -0400

    Updated start_rosserial.sh to set ROS_IP

--- roadkill/start_rosserial.sh
+++ roadkill/start_rosserial.sh
@@ -1,1 +1,5 @@
-rosrun rosserial_python serial_node.py /dev/ttyACM0
+echo WARNING: this script is not quite right for anything that isn't a FIT-PC.
+echo rosserial will continue trying to run in the backgroud, restarting itself if it dies.
+export ROS_IP=192.168.17.130
+watch rosrun rosserial_python serial_node.py /dev/ttyACM0 & 
+# watch rosrun rosserial_python serial_node.py /dev/ttyACM1 & 
