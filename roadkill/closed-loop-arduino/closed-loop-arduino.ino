b5a26345b99e2478b7cde06076b0df3785b4b56d
Eric Miller <eric@legoaces.org>
2015-10-28 20:18:03 -0400

    Minor tweaks (changed test constants)

--- roadkill/closed-loop-arduino/closed-loop-arduino.ino
+++ roadkill/closed-loop-arduino/closed-loop-arduino.ino
@@ -10,14 +10,15 @@ int val;    // variable to read the value from the analog pin
 void setup() 
 { 
   myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object 
-  
-} 
+  Serial.begin(9600);
+}
  
 void loop() 
 { 
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  Serial.
+  Serial.printf("Potentiometer value: %d\n", val);
 
-  myservo.write(val);                  // sets the servo position according to the scaled value 
+  myservo.write(300);                  // sets the servo position according to the scaled value 
   delay(15);                           // waits for the servo to get there 
 } 
+
