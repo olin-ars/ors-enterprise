712b3be898c54004a33dfe16eef44f218ca69dbc
Eric Miller <eric@legoaces.org>
2015-10-29 17:17:56 -0400

    Added logic for closed-loop control

--- roadkill/closed-loop-arduino/closed-loop-arduino.ino
+++ roadkill/closed-loop-arduino/closed-loop-arduino.ino
@@ -3,42 +3,87 @@
  * Prints "hello world!"
  */
 
-#include <ros.h>
-#include <std_msgs/Int16.h>
+ #include <ros.h>
+ #include <std_msgs/Int16.h>
 
-#include <Servo.h> 
+ #include <Servo.h> 
 
-ros::NodeHandle  nh;
+ #define SERVO_PIN 9
+ #define potpin A7
 
-std_msgs::Int16 pot_msg;
-ros::Publisher potentiometer("potentiometer", &pot_msg);
-
-char hello[13] = "hello world!";
-
-int i = 0;
-
-#define SERVO_PIN 9
+ ros::NodeHandle  nh;
 
 Servo myservo;  // create servo object to control a servo 
- 
-int potpin = A7;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin 
+
+int currentPos;    // variable to read the value from the analog pin 
+
+int SERVO_CENTER = 86;
+int lastCommanded = -1;
+
+std_msgs::Int16 pot_msg;
+ros::Publisher potentiometer("rudderSensor", &pot_msg);
+std_msgs::Int16 dir_msg;
+ros::Publisher direction_chn("rudderMotorDirection", &dir_msg);
+
+void commandCB(const std_msgs::Int16& command){
+    lastCommanded = command.data;
+}
+ros::Subscriber<std_msgs::Int16> commands("rudderCommands", &commandCB);
+
 
 void setup()
 {
   nh.initNode();
   nh.advertise(potentiometer);
+  nh.advertise(direction_chn);
+  nh.subscribe(commands);
 
   myservo.attach(SERVO_PIN);  // attaches the servo on pin SERVO_PIN to the servo object 
 }
 
+int movementDirection = 0; // 0 for stopped, 1 , -1 for current movement direction.
+
+void moveServo(){
+    const int power = 30;
+    static int currentTarget = lastCommanded;
+    if(lastCommanded < 0){
+        myservo.write(SERVO_CENTER);
+        return;
+    }
+
+    if (currentTarget != lastCommanded){
+        // A command has just been recieved
+        currentTarget = lastCommanded;
+        if (currentTarget > currentPos){
+            myservo.write(SERVO_CENTER + power);
+            movementDirection = 1;
+        }
+        else{
+            myservo.write(SERVO_CENTER - power);
+            movementDirection = -1;
+        }
+    }
+
+    if(
+        ((movementDirection == 1) && currentPos >= currentTarget) ||
+        ((movementDirection == -1) && currentPos <= currentTarget)){
+
+        myservo.write(SERVO_CENTER);
+        movementDirection == 0;
+    }
+    dir_msg.data = movementDirection;
+    direction_chn.publish( &dir_msg );
+}
+
 void loop()
 {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  currentPos = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
 
-  i++;
-  pot_msg.data = val;
+  pot_msg.data = currentPos;
   potentiometer.publish( &pot_msg );
+
+  moveServo();
+
   nh.spinOnce();
   delay(100);
 }
