4930a8c8c1ca27755349385709fa818b985e838e
Eric Miller <eric@legoaces.org>
2015-10-29 18:19:16 -0400

    bug fix

--- roadkill/start_rosserial.sh
+++ roadkill/start_rosserial.sh
@@ -1,4 +1,4 @@
-echo WARNING: this script is not quite right for anything that isn't a FIT-PC.
+echo WARNING: this script is not quite right for anything that is not a FIT-PC.
 echo rosserial will continue trying to run in the backgroud, restarting itself if it dies.
 export ROS_IP=192.168.17.130
 watch rosrun rosserial_python serial_node.py /dev/ttyACM0 & 
