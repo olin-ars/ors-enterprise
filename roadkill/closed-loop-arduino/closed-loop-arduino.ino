b1d6520537dd757aed092967f97b59ebf0e0d36e
Eric Miller <eric@legoaces.org>
2015-10-28 23:29:27 -0400

    FINALLY made potentiometer readings to ROS work. After 3.5 hrs.

--- roadkill/closed-loop-arduino/closed-loop-arduino.ino
+++ roadkill/closed-loop-arduino/closed-loop-arduino.ino
@@ -1,24 +1,44 @@
+/*
+ * rosserial Publisher Example
+ * Prints "hello world!"
+ */
+
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
 #include <Servo.h> 
- 
-#define SERVO_PIN 10
+
+ros::NodeHandle  nh;
+
+std_msgs::Int16 pot_msg;
+ros::Publisher potentiometer("potentiometer", &pot_msg);
+
+char hello[13] = "hello world!";
+
+int i = 0;
+
+#define SERVO_PIN 9
 
 Servo myservo;  // create servo object to control a servo 
  
-int potpin = 0;  // analog pin used to connect the potentiometer
+int potpin = A7;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin 
- 
-void setup() 
-{ 
-  myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object 
-  Serial.begin(9600);
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(potentiometer);
+
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin SERVO_PIN to the servo object 
 }
- 
-void loop() 
-{ 
+
+void loop()
+{
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  Serial.printf("Potentiometer value: %d\n", val);
 
-  myservo.write(300);                  // sets the servo position according to the scaled value 
-  delay(15);                           // waits for the servo to get there 
-} 
-
+  i++;
+  pot_msg.data = val;
+  potentiometer.publish( &pot_msg );
+  nh.spinOnce();
+  delay(100);
+}
