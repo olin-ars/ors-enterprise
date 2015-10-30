3507f01052ea3e28ee4849bfbae7c993681fd0cf
Eric Miller <eric@legoaces.org>
2015-10-30 08:08:10 -0400

    Reindented using tabs instead of an awkward mix of spaces and tabs

--- rudder_teensy/tests/closed-loop-arduino/closed-loop-arduino.ino
+++ rudder_teensy/tests/closed-loop-arduino/closed-loop-arduino.ino
@@ -31,68 +31,68 @@ std_msgs::Int16 dir_msg;
 ros::Publisher direction_chn("rudderMotorDirection", &dir_msg);
 
 void commandCB(const std_msgs::Int16& command){
-    if(command.data <= 0 || command.data >= 360){
-        return;
-    }
-    lastCommanded = command.data;
-    newCommand = true;
+	if(command.data <= 0 || command.data >= 360){
+		return;
+	}
+	lastCommanded = command.data;
+	newCommand = true;
 }
 ros::Subscriber<std_msgs::Int16> commands("rudderCommands", &commandCB);
 
 
 void setup()
 {
-  nh.initNode();
-  nh.advertise(potentiometer);
-  nh.advertise(direction_chn);
-  nh.subscribe(commands);
+	nh.initNode();
+	nh.advertise(potentiometer);
+	nh.advertise(direction_chn);
+	nh.subscribe(commands);
 
-  myservo.attach(SERVO_PIN);  // attaches the servo on pin SERVO_PIN to the servo object 
-  myservo.write(SERVO_CENTER);
+	myservo.attach(SERVO_PIN);  // attaches the servo on pin SERVO_PIN to the servo object 
+	myservo.write(SERVO_CENTER);
 }
 
 int movementDirection = 0; // 0 for stopped, 1 , -1 for current movement direction.
 
 void moveServo(){
-    const int power = 5;
+	const int power = 5;
 
-    if (newCommand){
-        // A command has just been recieved
-        newCommand = false;
-        if (lastCommanded > currentPos){
-            myservo.write(SERVO_CENTER + power);
-            movementDirection = 1;
-        }
-        else{
-            myservo.write(SERVO_CENTER - power);
-            movementDirection = -1;
-        }
-    }
+	if (newCommand){
+		// A command has just been recieved
+		newCommand = false;
+		if (lastCommanded > currentPos){
+			myservo.write(SERVO_CENTER + power);
+			movementDirection = 1;
+		}
+		else{
+			myservo.write(SERVO_CENTER - power);
+			movementDirection = -1;
+		}
+	}
 
-    if(
-        ((movementDirection == 1) && currentPos >= lastCommanded) ||
-        ((movementDirection == -1) && currentPos <= lastCommanded)){
-        // We have reached our target
-        myservo.write(SERVO_CENTER);
-        movementDirection = 0;
-    }
-    dir_msg.data = movementDirection;
-    direction_chn.publish( &dir_msg );
+	if(
+		((movementDirection == 1) && currentPos >= lastCommanded) ||
+		((movementDirection == -1) && currentPos <= lastCommanded)){
+		// We have reached our target
+		myservo.write(SERVO_CENTER);
+		movementDirection = 0;
+	}
+	dir_msg.data = movementDirection;
+	direction_chn.publish( &dir_msg );
 }
 
 float readPot(){
-    return 360 - analogRead(potpin) * (360.0 / 1024);  
+	return 360 - analogRead(potpin) * (360.0 / 1024);  
 }
 
 void loop()
 {
-  currentPos = readPot();          // reads the value of the potentiometer (value between 0 and 1023) 
+	currentPos = readPot();          // reads the value of the potentiometer (value between 0 and 1023) 
 
-  pot_msg.data = currentPos;
-  potentiometer.publish( &pot_msg );
+	pot_msg.data = currentPos;
+	potentiometer.publish( &pot_msg );
 
-  moveServo();
+	moveServo();
 
-  nh.spinOnce();
-  delay(10);
+	nh.spinOnce();
+	delay(10);
 }
