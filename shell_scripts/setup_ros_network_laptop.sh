62eeaabcd53ca04a21176ba44fe0dd007c24dba0
Eric Miller <eric@legoaces.org>
2015-10-29 21:29:34 -0400

    Created shell scripts to aid ROS network setup

---
+++ shell_scripts/setup_ros_network_laptop.sh
@@ -0,0 +1,11 @@
+# Because Olin's network doesn't handle hostnames properly, we need to tell ROS
+# to use IP addresses for communicating instead. To do this manually, use 
+# ifconfig or ipconfig to find the hostname.
+
+# Note that I think this script needs to be run in each terminal window you want to use.
+export ROS_IP=$(hostname --all-ip-addresses)
+
+
+# Also, because the laptop should not be running a copy of roscore during full operation, 
+# we need to tell it to look for Enterprise's ros server.
+export ROS_MASTER_URI=http://192.168.17.130:11311
