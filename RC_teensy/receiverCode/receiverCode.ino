f6a5d3f26a6d284651ec81ecba3fe38d64db66cc
Eric Miller <eric@legoaces.org>
2015-10-30 21:42:42 -0400

    Cleaned up PWM reading code.

---
+++ RC_teensy/receiverCode/receiverCode.ino
@@ -0,0 +1,34 @@
+int pins = 5;
+int piny = 3;
+int pinx = 4;
+
+float s, x, y;
+
+void setup() {
+  pinMode(pins, INPUT);
+  pinMode(pinx, INPUT);
+  pinMode(piny, INPUT);
+  Serial.begin(9600);
+
+}
+
+float readPWM(int pin, int range[2]){
+	int val = pulseIn(pin, HIGH);
+	float mid = (range[0] + range[1]) / 2.0;
+	float rng = (range[1] - range[0]) / 2.0;
+  return (val-(mid)) / rng;
+}
+
+void loop() {
+  int srange[] = {1150, 1700};
+  s = readPWM(pins, srange);
+  int xrange[] = {1180, 1730};
+  x = readPWM(pinx, xrange);
+  int yrange[] = {1180, 1720};
+  y = readPWM(piny, yrange);
+  //Serial.println(valy);
+  Serial.println(s);
+  Serial.println(x);
+  Serial.println(y);
+  delay(100);
+}
