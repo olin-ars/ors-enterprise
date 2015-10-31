181a5bd3f99e5a19f61b4204ad052217e3423f25
Eric Miller <eric@legoaces.org>
2015-10-30 21:51:03 -0400

    Converted recieverCode to ROS

--- RC_teensy/receiverCode/receiverCode.ino
+++ RC_teensy/receiverCode/receiverCode.ino
@@ -1,15 +1,33 @@
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+ros::NodeHandle  nh;
+
+std_msgs::Float32 x_msg;
+ros::Publisher x_pub("RC_x_axis", &x_msg);
+std_msgs::Float32 y_msg;
+ros::Publisher y_pub("RC_y_axis", &y_msg);
+std_msgs::Float32 s_msg;
+ros::Publisher s_pub("RC_s_axis", &s_msg);
+
 int pins = 5;
 int piny = 3;
 int pinx = 4;
 
 float s, x, y;
 
+void setupROS(){
+  nh.initNode();
+  nh.advertise(x_pub);
+  nh.advertise(y_pub);
+  nh.advertise(s_pub);
+}
+
 void setup() {
+  setupROS()
   pinMode(pins, INPUT);
   pinMode(pinx, INPUT);
   pinMode(piny, INPUT);
-  Serial.begin(9600);
-
 }
 
 float readPWM(int pin, int range[2]){
@@ -26,9 +44,14 @@ void loop() {
   x = readPWM(pinx, xrange);
   int yrange[] = {1180, 1720};
   y = readPWM(piny, yrange);
-  //Serial.println(valy);
-  Serial.println(s);
-  Serial.println(x);
-  Serial.println(y);
-  delay(100);
+
+  s_msg.data = s;
+  s_pub.publish(&s_msg);
+  x_msg.data = x;
+  x_pub.publish(&x_msg);
+  y_msg.data = y;
+  y_pub.publish(&y_msg);
+
+  nh.spinOnce();
+  delay(20);
 }
