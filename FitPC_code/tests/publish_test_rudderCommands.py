169d6446eb789976996fcf04389d627a220bada5
Eric Miller <eric@legoaces.org>
2015-10-30 20:36:34 -0400

    changed position target to random

--- FitPC_code/tests/publish_test_rudderCommands.py
+++ FitPC_code/tests/publish_test_rudderCommands.py
@@ -2,13 +2,14 @@
 import rospy
 from std_msgs.msg import Int16
 
+import random
 
 def talker():
     pub = rospy.Publisher('rudderCommands', Int16, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(0.2) # .2hz
     while not rospy.is_shutdown():
-        command = 100;
+        command = random.randint(30, 330);
         rospy.loginfo("Sent command {}".format(command))
         pub.publish(command)
         rate.sleep()
