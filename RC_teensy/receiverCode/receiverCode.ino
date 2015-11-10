a1b9e3cc3e5ab2618c52e9867270384d4e717a56
rdiverdi <Rocco.DiVerdi@students.olin.edu>
2015-11-09 20:15:18 -0500

    setup reciever code (not serial)

--- RC_teensy/receiverCode/receiverCode.ino
+++ RC_teensy/receiverCode/receiverCode.ino
@@ -1,49 +1,50 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 
 ros::NodeHandle nh;
 
-std_msgs::Float32 x_msg;
-ros::Publisher x_pub("RC_x_axis", &x_msg);
-std_msgs::Float32 y_msg;
-ros::Publisher y_pub("RC_y_axis", &y_msg);
-std_msgs::Float32 s_msg;
-ros::Publisher s_pub("RC_s_axis", &s_msg);
+std_msgs::Float32 Rudder_msg;
+ros::Publisher rudder_pub("RC_rudder_in", &Rudder_msg);
+std_msgs::Bool Switch_msg;
+ros::Publisher switch_pub("RC_switch_in", &Switch_msg);
+std_msgs::Float32 Sail_msg;
+ros::Publisher sail_pub("RC_sails_in", &Sail_msg);
 std_msgs::Float32 pot_msg;
 ros::Publisher pot_pub("potentiometer", &pot_msg);
 
 #define potpin A7
 
-int pins = 5;
-int piny = 3;
-int pinx = 4;
+int pinSails = 2;
+int pinRudder = 3;
+int pinSwitch = 6;
 
-float s, x, y;
+float SailCommand, RudderCommand, Switch;
 
 void setupROS(){
   nh.initNode();
-  nh.advertise(x_pub);
-  nh.advertise(y_pub);
-  nh.advertise(s_pub);
+  nh.advertise(rudder_pub);
+  nh.advertise(switch_pub);
+  nh.advertise(sail_pub);
   nh.advertise(pot_pub);
 }
 
 void setup() {
   setupROS();
-  pinMode(pins, INPUT);
-  pinMode(pinx, INPUT);
-  pinMode(piny, INPUT);
+  pinMode(pinSails, INPUT);
+  pinMode(pinRudder, INPUT);
+  pinMode(pinSwitch, INPUT);
 }
 
 float readPWM(int pin, int range[2]){
   // Timeout on pulsein is 50ms
-	int val = pulseIn(pin, HIGH, 50*1000); //NOTE: pulseIn() has pretty nasty resolution sometimes.
+  int val = pulseIn(pin, HIGH, 50*1000); //NOTE: pulseIn() has pretty nasty resolution sometimes.
   if (val == 0){
     // No pulse recieved, radio probably disconnected
     return 0.0;
   }
-	float mid = (range[0] + range[1]) / 2.0;
-	float rng = (range[1] - range[0]) / 2.0;
+  float mid = (range[0] + range[1]) / 2.0;
+  float rng = (range[1] - range[0]) / 2.0;
   return (val-mid) / rng;
 }
 
@@ -52,19 +53,19 @@ float readPot(){
 }
 
 void loop() {
-  int srange[] = {1150, 1700};
-  s = readPWM(pins, srange);
-  int xrange[] = {1180, 1730};
-  x = readPWM(pinx, xrange);
-  int yrange[] = {1180, 1720};
-  y = readPWM(piny, yrange);
+  int SailRange[] = {1150, 1700};
+  SailCommand = readPWM(pinSails, SailRange);
+  int RudderRange[] = {1180, 1730};
+  RudderCommand = readPWM(pinRudder, RudderRange);
+  int SwitchRange[] = {1180, 1720};
+  Switch = readPWM(pinSwitch, SwitchRange);
 
-  s_msg.data = s;
-  s_pub.publish(&s_msg);
-  x_msg.data = x;
-  x_pub.publish(&x_msg);
-  y_msg.data = y;
-  y_pub.publish(&y_msg);
+  Sail_msg.data = SailCommand;
+  sail_pub.publish(&Sail_msg);
+  Rudder_msg.data = RudderCommand;
+  rudder_pub.publish(&Rudder_msg);
+  Switch_msg.data = Switch < 0;
+  switch_pub.publish(&Switch_msg);
 
   pot_msg.data = readPot();
   pot_pub.publish(&pot_msg);
