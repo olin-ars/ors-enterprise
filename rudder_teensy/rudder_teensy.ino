d8fc34014ce8cec8a9be119b89b2a3b54ee2b926
yycho0108 <jchocholate@gmail.com>
2015-11-11 21:33:18 -0500

    Added Limit Switch Code

--- rudder_teensy/rudder_teensy.ino
+++ rudder_teensy/rudder_teensy.ino
@@ -15,7 +15,13 @@
 #define SERVO_PIN 9
 #define potpin A7
 
-ros::NodeHandle  nh;
+#define STAR_LIM_PIN 11 //StarBoard
+#define PORT_LIM_PIN 12 //Port
+//Assumption : STAR = (numerically) bigger angle, PORT = less angle
+
+int softLimStop = 0;
+
+ros::NodeHandle nh;
 
 Servo myservo;  // create servo object to control a servo 
 
@@ -32,11 +38,11 @@ std_msgs::Int16 dir_msg;
 ros::Publisher dir_pub("rudderMotorDirection", &dir_msg);
 
 void command_callback(const std_msgs::Int16& command){
-	if(command.data <= 0 || command.data >= 360){
-		return;
-	}
-	lastCommanded = command.data;
-	newCommand = true;
+  if(command.data <= 0 || command.data >= 360){
+    return;
+  }
+  lastCommanded = command.data;
+  newCommand = true;
 }
 ros::Subscriber<std_msgs::Int16> command_sub("rudderCommands", &command_callback);
 
@@ -44,49 +50,77 @@ void center_callback(const std_msgs::Int16& msg){SERVO_CENTER = msg.data;}
 ros::Subscriber<std_msgs::Int16> center_sub("rudderServoCenter", &center_callback);
 
 void setupROS(){
-	nh.initNode();
-	nh.advertise(pot_pub);
-	nh.advertise(dir_pub);
-	nh.subscribe(command_sub);
-	nh.subscribe(center_sub);
+  nh.initNode();
+  nh.advertise(pot_pub);
+  nh.advertise(dir_pub);
+  nh.subscribe(command_sub);
+  nh.subscribe(center_sub);
 }
 
 void setup()
 {
-	setupROS();
-
-	myservo.attach(SERVO_PIN);  // attaches the servo on pin SERVO_PIN to the servo object 
-	myservo.write(SERVO_CENTER);
+  setupROS();
+  Serial.begin(9600);
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin SERVO_PIN to the servo object 
+  myservo.write(SERVO_CENTER);
+  pinMode(STAR_LIM_PIN,INPUT);
+  pinMode(PORT_LIM_PIN,INPUT);
 }
 
 int movementDirection = 0; // 0 for stopped, 1 , -1 for current movement direction.
 
 void moveServo(){
-	const int power = 5;
+  const int power = 5;
 
-	if (newCommand){
-		// A command has just been recieved
-		newCommand = false;
-		if (lastCommanded > currentPos){
-			myservo.write(SERVO_CENTER + power);
-			movementDirection = 1;
-		}
-		else{
-			myservo.write(SERVO_CENTER - power);
-			movementDirection = -1;
-		}
-	}
+  if (newCommand){
 
-	if(abs(currentPos - lastCommanded) <= DEADZONE){
-		// We have reached our target
-		myservo.write(SERVO_CENTER);
-		movementDirection = 0;
-	}
-	dir_msg.data = movementDirection;
+    //check limits
+    if((softLimStop == STAR_LIM_PIN && lastCommanded >= currentPos) //STAR = bigger angle, PORT = less angle
+        ||(softLimStop == PORT_LIM_PIN && lastCommanded <= currentPos)){
+          //when command exceeding the limits are issued
+          newCommand = false; // invalidate command
+          lastCommanded = currentPos;
+    }
+    else{
+    softLimStop = 0; //release Soft Limit Switch
+    }
+    
+    // A valid command has just been recieved
+    if(newCommand){
+      newCommand = false;
+      if (lastCommanded > currentPos){
+        myservo.write(SERVO_CENTER + power);
+        movementDirection = 1;
+      }
+      else{
+        myservo.write(SERVO_CENTER - power);
+        movementDirection = -1;
+      }
+    }
+  }
+  
+  if(softLimStop){
+    lastCommanded = currentPos; //stop servo
+  }
+  
+  if(abs(currentPos - lastCommanded) <= DEADZONE){
+    // We have reached our target
+    myservo.write(SERVO_CENTER);
+    movementDirection = 0;
+  }
+  dir_msg.data = movementDirection;
 }
 
 float readPot(){
-	return 360 - analogRead(potpin) * (360.0 / 1024);  
+  return 360 - analogRead(potpin) * (360.0 / 1024);  
+}
+bool readLim(){
+  //stop if TRUE
+  if(digitalRead(STAR_LIM_PIN) == HIGH)
+    return STAR_LIM_PIN;
+  else if(digitalRead(PORT_LIM_PIN) == HIGH)
+    return PORT_LIM_PIN;
+   return 0;
 }
 
 // Controls the frequency with which ROS transmits/recieves data.
@@ -96,20 +130,21 @@ unsigned int ros_transmit_period = 10; //milliseconds
 
 void loop()
 {
-	currentPos = readPot();          // reads the value of the pot_pub (value between 0 and 1023) 
+  currentPos = readPot();          // reads the value of the pot_pub (value between 0 and 1023) 
+  softLimStop = readLim();
 
-	pot_msg.data = currentPos;
+  pot_msg.data = currentPos;
 
-	static unsigned long last_ros_transmit = millis();
-	if (millis() - last_ros_transmit >= ros_transmit_period){
-		last_ros_transmit = millis();
+  static unsigned long last_ros_transmit = millis();
+  if (millis() - last_ros_transmit >= ros_transmit_period){
+    last_ros_transmit = millis();
 
-		dir_pub.publish( &dir_msg );
-		pot_pub.publish( &pot_msg );
-		nh.spinOnce();
-	}
+    dir_pub.publish( &dir_msg );
+    pot_pub.publish( &pot_msg );
+    nh.spinOnce();
+  }
+    
+  moveServo();
 
-	moveServo();
-
-	delay(1);
+  delay(1);
 }
