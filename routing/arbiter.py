fc757f44ad6019b81925f4951de37a5d9ea4a709
yycho0108 <jchocholate@gmail.com>
2016-02-02 03:31:35 +0000

    Arbiter Listens to correct messages

--- routing/arbiter.py
+++ routing/arbiter.py
@@ -5,55 +5,60 @@ DEFAULT = 0
 RC_MODE = 1
 AUTO_MODE = 2
 TEST_MODE = 3
+Modes = [DEFAULT,RC_MODE,AUTO_MODE,TEST_MODE]
 
 class Arbiter:
-	def __init__(self):
-            self.rudder=0
-            self.sail=0
-            self.controlType = 0 #DEFAULT = 0
-            self.debugDial = 0
-            #output
-            self.rudderPub = rospy.Publisher('rudder/set_point', Int16)
-            self.sailPub = rospy.Publisher('sail/set_point', Int16)
+    def __init__(self):
+        self.rudder=0
+        self.sail=0
+        self.opMode = 0 #DEFAULT = 0
+        self.debugDial = 0
+        #output
+        self.rudderPub = rospy.Publisher('rudder/set_point', Int16)
+        self.sailPub = rospy.Publisher('sail/set_point', Int16)
 
-            #input
-            self.rudderSub = rospy.Subscriber('rc/rudder_in', Float32, self.onRCRudder) #listening to rudder_in
-            self.switchSub = rospy.Subscriber('rc/switch_in', Bool, self.onRCSwitch) #listening to swith_in
-            self.sailsSub = rospy.Subscriber('rc/sails_in', Float32, self.onRCSail) #listening to sails_in
-            self.debugSub = rospy.Subscriber('rc/debug_dial_in', Float32, self.onDebugDial)
-            #autoRudderSub = rospy.Subscriber('auto_rudder_in', Float32)
-            #autoSailsSub = rospy.Subscriber('auto_sails_in', Float32)
-            controlType = rospy.Subscriber('control_type', Int16,self.onControlType)
-            #for further expansion
+        self.rudderSub = None
+        #autoRudderSub = rospy.Subscriber('auto_rudder_in', Float32)
+        #autoSailsSub = rospy.Subscriber('auto_sails_in', Float32)
+        self.opModeSub = rospy.Subscriber('operating_mode', Int16,self.onOpMode)
+        #for further expansion
+        setupSubscribers(self,DEFAULT)
 
-	def onRCRudder(self,data):
-            self.rudder=data
-            if self.controlType == RC_MODE:
-                self.rudderPub.publish(data) #else ignore
-	def onRCSwitch(self,data):
-            if data is True:
-                self.controlType = RC_MODE
-            else:
-                self.controlType = DEFAULT
-	def onRCSail(self,data):
-            self.sail=data
-            if self.controlType == RC_MODE:
-                self.sailPub.publish(data)
-	def onAutoRudder(self,data):
-            self.rudder=data
-            if self.controlType == AUTO_MODE:
-                self.rudderPub.publish(data)
-	def onAutoSail(self,data):
-            self.sail=data
-            if self.controlType == AUTO_MODE:
-                self.rudderPub.publish(data)
-	def onControlType(self,data):
-            self.controlType = data
-        def onDebugDial(self,data):
-            self.debugDial = data
-            #currently the purpose of potentiometer is not defined?
-	def spin(self):
-            rospy.spin()
+    def setupSubscribers(self, mode):
+        namespaces = {DEFAULT: None, RC_MODE: "rc_mode", AUTO_MODE:None, TEST_MODE:"test_mode"}
+        currentNamespace = namespaces[mode]
+
+        #input
+        if rudderSub:
+            self.rudderSub.unregister();
+            self.sailsSub.unregister();
+
+        if not currentNamespace:
+            return
+
+        self.rudderSub = rospy.Subscriber(currentNamespace + '/rudder/set_point', Int16, self.onRudder) #listening to rudder_in
+        self.sailsSub = rospy.Subscriber(currentNamespace + '/sail/set_point', Int16, self.onSail) #listening to sails_in
+    
+    def onRudder(self,data):
+        self.rudder=data
+        self.rudderPub.publish(data)
+
+#    def onRCSwitch(self,data):
+#        if data is True:
+#            self.controlType = RC_MODE
+#        else:
+#            self.controlType = DEFAULT
+#
+    def onSail(self,data):
+        self.sail=data
+        self.sailPub.publish(data)
+
+    def onOpMode(self,data):
+        self.opMode = data
+        setupSubscribers(self,data)
+
+    def spin(self):
+        rospy.spin()
 
 if __name__ == '__main__':
 	arbiter = new Arbiter()
