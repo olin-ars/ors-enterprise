d5f8051f799b9a6737d0ec76d8d924d9cf47ae1f
Eric Miller <eric@legoaces.org>
2016-03-11 20:19:47 -0500

    Removed undefined main_angle, which isn't optimal, but sort of works

--- fit_pc_pkg/scripts/simulator/simulator_main.py
+++ fit_pc_pkg/scripts/simulator/simulator_main.py
@@ -126,7 +126,7 @@ class Boat:
         #rudder torque aspect
         Tr = -self.kw*math.log(abs(self.forward_speed)+1)*self.RudderPos #all of these ratios are made up 
         #sail torque aspect
-        Ts = -self.q*sin(self.main_angle-model.wind.windheading)*sqrt(abs(model.wind.windspeed))
+        Ts = -self.q*sin(self.heading-model.wind.windheading)*sqrt(abs(model.wind.windspeed))
         # Ts = 0 #!!!
         #Ts = self.q*self.strength_Jib*sin(self.jib_angle-model.wind.windheading)*sqrt(abs(model.wind.windspeed))
         #this overwrite is an error and needs to be addressed, however, it works decently with it.
