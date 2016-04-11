b5807453999bd3bd711f837607b77365e8b120cd
Andrew Pan <andrew.pan@students.olin.edu>
2016-04-11 19:58:52 -0400

    slight change to rudder publisher, there isn't a rudder branch soooo...

--- rudder_autonomy/auto_rudder_publisher.py
+++ rudder_autonomy/auto_rudder_publisher.py
@@ -4,10 +4,12 @@ from std_msgs.msg import Int16, Float32
 
 class autonomousRudderPublisher:#needs a topic to read in waypoint angle from
 	def __init__(self):
-        self.rudderPub = rospy.Publisher('rc_mode/rudder/set_point', Int16)
-        def callback(angle):
-        	self.waypoint = angle
-        self.waypointAngleListener = rospy.Subscriber('waypointAngle', Int16, callback) #needs somewhere to get waypoint
+		rospy.init_node('rudder_publisher')
+		self.rudderPub = rospy.Publisher('rc_mode/rudder/set_point', Int16)
+		self.waypoint = 0
+		def callback(angle):
+			self.waypoint = angle
+		self.waypointAngleListener = rospy.Subscriber('waypointAngle', Int16, callback) #needs somewhere to get waypoint
 	def calculateRudderPosition(self, waypointAngle):#calculate position rudder needs to be at to direct self at current waypoint angle, relative to bow of the boat as 0 degrees, positive is starboard and negative is port
 		#currently nobody has defined rudder controls
 		#rudder messages currently range from -45 to 45, assuming positive is starboard and negative is port
@@ -16,11 +18,11 @@ class autonomousRudderPublisher:#needs a topic to read in waypoint angle from
 
 	def run(self):
 		rate = rospy.Rate(10)  # 10hz
-        while not rospy.is_shutdown():
-        	rud = calculateRudderPosition(self.waypoint)
-        	self.rudderPub.publish(rud)
-        	rospy.loginfo("Sent desired rudder angle : {}".format(rud))
-        	rate.sleep()
+		while not rospy.is_shutdown():
+			rud = self.calculateRudderPosition(self.waypoint)
+			self.rudderPub.publish(rud)
+			rospy.loginfo("Sent desired rudder angle : {}".format(rud))
+			rate.sleep()
 
 if __name__ == '__main__':
 	try:
