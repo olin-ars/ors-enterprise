8b252b21beba445d90fc3589b3df74002f09a206
Eric Miller <eric@legoaces.org>
2016-04-11 21:48:18 -0400

    Added publishers and subscribers for boat velocity

--- fit_pc_pkg/scripts/sensor_middleman.py
+++ fit_pc_pkg/scripts/sensor_middleman.py
@@ -13,7 +13,9 @@ class Arbiter:
 
         self.home = home
         self.pos = home
-        self.heading = 0
+        self.heading = 0.0
+        self.speed = 0.0
+        self.track = 0.0
 
         self.initScalars()
 
@@ -34,18 +36,29 @@ class Arbiter:
 
     def initPublishers(self):
         self.posPub = rospy.Publisher('location', Pose2D)
+        self.velPub = rospy.Publisher('velocity', Pose2D)
 
     def initSubscribers(self):
         self.positionSub = rospy.Subscriber('/hemishphere/position', Pose2D, self.onPosition)
+        self.speedSub = rospy.Subscriber('/hemishphere/speed', Float32, self.onSpeed)
+        self.trackSub = rospy.Subscriber('/hemishphere/track', Float32, self.onTrack)
 
     def onPosition(self, msg):
         # Incomming messages are in decimal minutes
-
         self.pos = self.transformLocation(msg.x / 60, msg.y / 60)
         self.heading = msg.theta
 
+    def onSpeed(self, msg):
+        # Incomming messages are in decimal minutes
+        self.speed = msg.data
+
+    def onTrack(self, msg):
+        # Incomming messages are in degrees clockwise of North
+        self.track = msg.data
+
     def publish(self):
         self.posPub(Pose2D(self.pos[0], self.pos[1], self.heading))
+        self.velPub(Pose2D(self.speed, 0.0, self.track))
 
     def run(self):
         rate = rospy.Rate(10)  # 10hz
