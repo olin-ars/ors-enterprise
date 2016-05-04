75686c933eabc192a0e62a548cd116d4c1cb1398
Eric Miller <eric@legoaces.org>
2016-05-04 17:29:37 -0400

    Finished first version of wind data translation in sensor_middleman

--- fit_pc_pkg/scripts/sensor_middleman.py
+++ fit_pc_pkg/scripts/sensor_middleman.py
@@ -4,6 +4,18 @@ import math
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Pose2D, Vector3
 
+knots = 0.514444  # 1 knot in m/s
+
+
+def angle_range(a):
+    """ limit angles to range of -180 to 180 """
+    return (a + 180) % 360 - 180
+
+
+def subtract_angles(a, b):
+    """ subtract angles while keeping them in range -180 to 180 """
+    return angle_range(a - b)
+
 
 class Arbiter:
     def __init__(self, home=(42.293441, -71.263970)):
@@ -15,6 +27,9 @@ class Arbiter:
         self.heading = 0.0
         self.speed = 0.0
         self.track = 0.0
+
+        self.relwind = (0, 0)  # (direction, speed)
+        self.truewind = (0, 0)  # (direction, speed)
 
         self.initScalars()
 
@@ -42,15 +57,35 @@ class Arbiter:
         self.posPub = rospy.Publisher('location', Pose2D, queue_size=queue)
         self.velPub = rospy.Publisher('velocity', Pose2D, queue_size=queue)
 
+        self.relWindPub = rospy.Publisher(
+            'relative_wind', Pose2D, queue_size=queue)
+        self.trueWindPub = rospy.Publisher(
+            'true_wind', Pose2D, queue_size=queue)
+        self.globalWindPub = rospy.Publisher(
+            'global_wind', Pose2D, queue_size=queue)
+
     def initSubscribers(self):
         # Hemisphere subscribers
-        self.positionSub = rospy.Subscriber('/hemisphere/position', Pose2D, self.onPosition)
-        self.speedSub = rospy.Subscriber('/hemisphere/speed', Float32, self.onSpeed)
-        self.trackSub = rospy.Subscriber('/hemisphere/track', Float32, self.onTrack)
+        self.positionSub = rospy.Subscriber('/hemisphere/position', Pose2D,
+                                            self.onPosition)
+        self.speedSub = rospy.Subscriber('/hemisphere/speed', Float32,
+                                         self.onSpeed)
+        self.trackSub = rospy.Subscriber('/hemisphere/track', Float32,
+                                         self.onTrack)
 
         # Airmar subscribers
-        self.relWindSub  = rospy.Subscriber('airmar/relative_wind', Vector3, self.onRelWind)
-        self.trueWindSub = rospy.Subscriber('airmar/true_wind', Vector3, self.onTrueWind)
+        self.relWindSub = rospy.Subscriber('airmar/relative_wind', Vector3,
+                                           self.onRelWind)
+        self.trueWindSub = rospy.Subscriber('airmar/true_wind', Vector3,
+                                            self.onTrueWind)
+
+    def onRelWind(self, msg):
+        self.relwind[0] = msg.x
+        self.relwind[1] = msg.y * knots
+
+    def onTrueWind(self, msg):
+        self.truewind[0] = msg.x
+        self.truewind[1] = msg.y * knots
 
     def onPosition(self, msg):
         # Incomming messages are in decimal degrees
@@ -68,6 +103,17 @@ class Arbiter:
     def publish(self):
         self.posPub.publish(Pose2D(self.pos[1], self.pos[0], self.heading))
         self.velPub.publish(Pose2D(self.speed, 0.0, self.track))
+
+        self.relWindPub.publish(
+            Pose2D(self.relwind[1], 0.0, self.relwind[0]))
+        self.trueWindPub.publish(
+            Pose2D(self.truewind[1], 0.0, self.truewind[0]))
+
+        # Be warned, there could be a negative mistake in the following code.
+        # Review needed.
+        self.globalWindPub.publish(
+            Pose2D(self.relwind[1], 0.0,
+                   subtract_angles(self.relwind[0], self.heading)))
 
     def run(self):
         rate = rospy.Rate(10)  # 10hz
