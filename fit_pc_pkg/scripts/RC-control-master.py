9261a97a48d53ead2c324f1b7fc07cb111913c05
vickymmcd <vickymmcd@gmail.com>
2016-10-19 20:51:58 -0400

    RC Comments Made

--- fit_pc_pkg/scripts/RC-control-master.py
+++ fit_pc_pkg/scripts/RC-control-master.py
@@ -8,13 +8,16 @@ class RCmaster(object):
         rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
             # Convert (-1.0 -- 1.0) to (-45 -- 45)
+            #publishes the rudder position in a readable format to ROS
             rudderMsg = int(self.rudderPos * 45)
             self.rudderPub.publish(rudderMsg)
 
             # Convert (-1.0 -- 1.0) to (0 -- 6)
+            #publishes the sail position in a readable format to ROS
             sailMsg = int((self.sailPos + 1.0) * 6/2.0)
             self.sailPub.publish(sailMsg)
 
+            #logging sent info
             rospy.loginfo("Sent command (Rudder, Sail)=({}, {})".format(rudderMsg, sailMsg))
 
             rate.sleep()
@@ -34,9 +37,11 @@ class RCmaster(object):
         self.rudderSub = rospy.Subscriber('rc/rudder_in', Float32, self.update_rudder)
 
     def update_rudder(self, msg):
+        #updates the rudder position
         self.rudderPos = msg.data
 
     def update_sail(self, msg):
+        #updates the sail position
         self.sailPos = msg.data
 
 
